f0da4de8840b423e808f5b267430a6c8bdb36470
Roy Carlos Huayre Bujaico <rhuayre@tumi-soft.com>
2021-12-17 18:38:18 -0500

    implementacion de login

---
+++ app/Http/Controllers/admin/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('home');
+        }
+
+        return back()->withErrors([
+            'error' => 'Credenciales incorrectos.',
+        ]);
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('login');
+    }
+}
